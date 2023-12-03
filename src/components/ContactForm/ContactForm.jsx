7996b22a1be87b996cf301649c971058d5b86cc3
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-12-03 22:06:42 +0100

    redo

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,31 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-
 import cssform from '../ContactForm/ContactForm.module.css';
 
 export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   handleFormSubmit = event => {
     event.preventDefault();
 
-    const name = event.target.name.value;
-    const number = event.target.number.value;
+    const { name, number } = this.state;
     const { addContact } = this.props;
 
     addContact({ id: nanoid(), name, number });
-    event.target.reset();
+    this.setState({ name: '', number: '' });
   };
-    
-     render() {
+
+  render() {
+    const { name, number } = this.state;
+
     return (
       <section className={cssform.form}>
         <h1 className={cssform.form__title}>Phonebook</h1>
@@ -27,6 +37,9 @@ export class ContactForm extends Component {
             className={cssform.form__input}
             placeholder="Enter name"
             required
+            value={name}
+            onChange={this.handleInputChange}
+            
           />
           <label className={cssform.form__label}>Number</label>
           <input
@@ -35,6 +48,8 @@ export class ContactForm extends Component {
             className={cssform.form__input}
             placeholder="Enter phone number"
             required
+            value={number}
+            onChange={this.handleInputChange}
           />
           <button className={cssform.form__btn} type="submit">
             Add contact
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -20,5 +20,3 @@ export class Filter extends Component {
   }
 }
 
-
-
